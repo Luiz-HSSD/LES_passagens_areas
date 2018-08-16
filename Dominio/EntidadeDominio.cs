3d32565d18f0e67b93d6644b78b45544b3b4235e
Luiz <lhenrique_diniz@hotmail.com>
2018-08-15 23:28:35 -0300

    first alteration

---
+++ Dominio/EntidadeDominio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class EntidadeDominio
+    {
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public EntidadeDominio()
+        {
+            _id = 0;
+        }
+    }
+}
